d7e621cf0740814b171ea2278cbe3e4963c2afd9
Eric Wu <ericwu692@yahoo.com>
2021-07-21 22:45:33 -0400

    added dp longestsubseq

---
+++ dynamicProgramming/longestSubsequence/longestSubsequenceTopDownDP.java
@@ -0,0 +1,21 @@
+class Solution {
+    public Integer[][] dp;
+    public int longestCommonSubsequence(String text1, String text2) {
+        dp = new Integer[text1.length()][text2.length()];
+        return recursiveHelper(text1, text2, 0 , 0);
+    }
+    
+    public int recursiveHelper(String text1, String text2, int i, int j){
+        if(i == text1.length() || j == text2.length()){
+            return 0;
+        }
+        if(dp[i][j] != null) {
+            return dp[i][j];
+        }
+        if(text1.charAt(i) == text2.charAt(j)){
+            return dp[i][j] = 1 + recursiveHelper(text1, text2, i+1, j+1);
+        }
+        return dp[i][j] = Math.max(recursiveHelper(text1, text2, i+1, j), 
+                        recursiveHelper(text1, text2, i, j+1));
+    }
+}
